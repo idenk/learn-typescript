4d36ff0cf981079568ee2fe6fb0e5fee27fa77c1
idenk <idenkim@gmail.com>
2021-08-02 20:43:44 +0900

    타입스크립트 소개와 배경

---
+++ why-ts/sample.js
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// var result = sum(10, 20);
+// result.toLocaleString();
+
+
+// @ts-check 에러 메시지 출력
+// 자바스크립트를 타입스크립트처럼 쓰는 방법 (타입 검증, 보완) -> 가독성 하락, 재활용과 확장 관점에서 좋지 않음
+/**
+ * JSDoc
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
+  return a + b;
+}
+// sum(10, '20');  // 오류
---
+++ why-ts/sample.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+var result = add(10, 20);
+result.toLocaleString();
+
+
+// add(19, '20');
